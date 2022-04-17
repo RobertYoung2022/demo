edf8f300c898f80f41e3bcfb7137078507fdfe27
Robert Young <94646951+RobertYoung2022@users.noreply.github.com>
2022-04-17 13:39:27 -0500

    created Student class

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,28 +1,44 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
-	// method
+    // method
 //	@GetMapping // <- GetMapping + the @RestController makes this class into an RESTful method to GET some data
 //	public String hello() {
 //		return "Hello World";
 //	}
 
-	@GetMapping
-	public List<String> helloList() {
-		return List.of("Hello", "World");
-	}
+//	@GetMapping
+//	public List<String> helloList() {
+//		return List.of("Hello", "World");
+//	}
+
+    @GetMapping
+    public List<Student> studentList() {
+        return List.of(
+                new Student(
+                        1L,
+                        "Lupe",
+                        "lupe@her@gkail.io",
+                        LocalDate.of(2000, Month.OCTOBER, 18),
+                        18
+
+                ));
+    }
 }
