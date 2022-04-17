7093048449b27749fe30e3bb58939186e9b68ca7
Robert Young <94646951+RobertYoung2022@users.noreply.github.com>
2022-04-17 13:52:19 -0500

    created api layer: StudentController

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,10 @@
 package com.example.demo;
 
-import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
@@ -11,7 +12,9 @@ import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
-@RestController
+@RestController //
+
+@RequestMapping(path = "/home")
 public class DemoApplication {
 
     public static void main(String[] args) {
@@ -19,26 +22,27 @@ public class DemoApplication {
     }
 
     // method
-//	@GetMapping // <- GetMapping + the @RestController makes this class into an RESTful method to GET some data
-//	public String hello() {
-//		return "Hello World";
-//	}
+	@GetMapping // <- GetMapping + the @RestController makes this class into an RESTful method to GET some data
+	public String hello() {
+		return "Hello World";
+	}
 
 //	@GetMapping
 //	public List<String> helloList() {
 //		return List.of("Hello", "World");
 //	}
 
-    @GetMapping
-    public List<Student> studentList() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Lupe",
-                        "lupe@her@gkail.io",
-                        LocalDate.of(2000, Month.OCTOBER, 18),
-                        18
-
-                ));
-    }
+//    @GetMapping
+//    public List<Student> studentList() {
+//        return List.of(
+//                new Student(
+//                        1L,
+//                        "Lupe",
+//                        "lupe@her@gkail.io",
+//                        LocalDate.of(2000, Month.OCTOBER, 18),
+//                        18
+//
+//                )
+//        );
+//    }
 }
